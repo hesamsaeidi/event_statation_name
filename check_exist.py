991066dceb87f4f45affc0d07e606a5ddca34477
Hesam <hsaeidi@crimson.ua.edu>
2020-06-08 09:27:57 -0500

    check_exist.py developed with square search and time check but still no insertion to the db

--- check_exist.py
+++ check_exist.py
@@ -2,6 +2,7 @@ from query import database_query
 from query import select_query
 import datetime
 import math
+import time
 
 
 
@@ -10,28 +11,20 @@ def my_round(x):
     return round(x*4)/4
 
 def lat_lon_conv(lat, lon):
+    # lat and lon stored in master dataset as colatitude (0-180) and positive longitude (0-360)
+    # for the sake of comparison this function convert the input lat and lon to the desired values
     co_lat = 90 - float(lat)
     new_lon = (float(lon) + 360) % 360
     return co_lat, new_lon
 
-
+t_delta = datetime.timedelta(minutes=10)
 # defining the db in use
 my_db = '/Users/hesam/test/db_test/all_data.db'
-
- # defining the query based on finding the location only
-# my_query = '''SELECT
-#                  id, e_id, date_time, lat, lon, depth
-#              from
-#                  events
-#              where
-#                  round(lat, 1) = {}
-#                  and
-#                  round(lon, 1) = {}'''
 
 # query of searching for location needs 4 int as a square search
 # in this order: min_lat, max_lat, min_lon, max_lon
 my_query = """SELECT
-                    COUNT(*) id
+                    id, e_id, lat, lon, date_time
                 FROM
                     events
                 WHERE
@@ -44,7 +37,7 @@ my_query = """SELECT
                     AND '{}'"""
 
 
-with open('1B_events.txt', "r") as f:
+with open('1C_events.txt', "r") as f:
     newEvents = f.readlines()
 
     for entry in newEvents:
@@ -52,28 +45,27 @@ with open('1B_events.txt', "r") as f:
 
         # create datetime obj for newEvents
         temp_date = [int(float(x)) for x in raw_list[0:6]]
-        raw_dt_obj = datetime.datetime(*temp_date)
+        entry_dt_obj = datetime.datetime(*temp_date)
 
         # split lat and lon
         entry_lat , entry_lon = lat_lon_conv(*raw_list[6:8])
+
+        # create the square search criteria
         min_lat = math.floor(entry_lat)
         max_lat = math.ceil(entry_lat)
         min_lon = math.floor(entry_lon)
         max_lon = math.ceil(entry_lon)
-        # print(raw_list[6:8], "<<<<rrr>>>>",entry_lat, entry_lon, "\n",min_lat, max_lat, min_lon, max_lon)
 
 
 
         location_search_query = my_query.format(min_lat, max_lat, min_lon, max_lon)
         search_query = select_query(my_db, location_search_query)
-        print(search_query)
+        for q in search_query:
+            try:
+                q_dt_obj = datetime.datetime.strptime(q[-1], '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError:
+                print(">>> ValueError for 60 seconds or 60 minutes:")
+                q_dt_obj = datetime.datetime.fromtimestamp(time.mktime(time.strptime(q[-1], '%Y-%m-%d %H:%M:%S.%f')))
 
-
-
-
-# resutl = select_query(my_db, my_query)
-# print(resutl)
-#
-#
-# datetimeObj = datetime.datetime.strptime(resutl[0][1], '%Y-%m-%d %H:%M:%S.%f')
-# print(">>>", datetimeObj)
+            if t_delta > abs(q_dt_obj - entry_dt_obj):
+                print(q[0:], entry, entry_dt_obj)
