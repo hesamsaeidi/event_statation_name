7dfaa6591e7d8055b44d435abb235f8b14658119
hesamsaeidi <66099111+hesamsaeidi@users.noreply.github.com>
2021-06-22 23:25:04 -0500

    minor changes

--- data_migrator.py
+++ data_migrator.py
@@ -21,13 +21,27 @@ import shutil
 #             destin.format(dest_dir='EVENTS',netwrok_name = net, file_name = "events.txt")
 #         )
 
-
-dir_path = '/Users/hesam/test/db_test/NETWORKS/EVT_WNUM/'
-bigList = os.listdir(dir_path)
-destin = "/Users/hesam/AFRICA/{netwrok_name}/events_wNums.txt"
-
-for f in bigList:
-    network_code = f.split('_')[0]
-    shutil.copy(dir_path+f, destin.format(netwrok_name=network_code))
+#
+# dir_path = '/Users/hesam/test/db_test/NETWORKS/EVT_WNUM/'
+# bigList = os.listdir(dir_path)
+# destin = "/Users/hesam/AFRICA/{netwrok_name}/events_wNums.txt"
+#
+# for f in bigList:
+#     network_code = f.split('_')[0]
+#     shutil.copy(dir_path+f, destin.format(netwrok_name=network_code))
 
     # print(dir_path+f)
+
+
+dir_path = '/Users/hesam/AFRICA'
+bigList = os.listdir(dir_path)
+source = "/Users/hesam/AFRICA/{netwrok_name}/allData.out"
+destin = "/Users/hesam/test/db_test/NETWORKS/ALLDATA/{netwrok_name}_allData.out"
+
+for net in bigList:
+    if net != "SCRIPTS" and net != ".DS_Store":
+            shutil.copy(
+                source.format(netwrok_name = net)
+                ,
+                destin.format(netwrok_name = net)
+            )
